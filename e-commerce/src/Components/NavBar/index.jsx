e15cbed7d038624b01f2094ce1556f61096f8be4
gianluca <gianlucaferrari243@gmail.com>
2022-10-30 23:39:38 -0300

    pf2

---
+++ e-commerce/src/Components/NavBar/index.jsx
@@ -0,0 +1,27 @@
+
+import React from 'react';
+
+export const NavBar = () => {
+    return (
+        <div className="container">
+            <nav className="nav">
+                <div className="nav__brand">
+                    <a className="nav__link" href="#">MiMarca</a>
+                </div>
+                <ul className="nav__list">
+                    <li>
+                        <a className="nav__links" href="#">Categoria1</a>
+                    </li>
+                    <li>
+                        <a className="nav__links" href="#">Categoria2</a>
+                    </li>
+                    <li>
+                        <a className="nav__links" href="#">Carrito</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default NavBar;
